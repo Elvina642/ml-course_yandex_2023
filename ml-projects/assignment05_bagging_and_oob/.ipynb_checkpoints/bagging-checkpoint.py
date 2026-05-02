78446711f6e099a564dd79248521ea0918ecf804
Эльвина <elvina.yafarova2016@gmail.com>
2026-05-02 21:42:41 +0400

    restructure: move projects into ml-projects folder

---
+++ ml-projects/assignment05_bagging_and_oob/.ipynb_checkpoints/bagging-checkpoint.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+class SimplifiedBaggingRegressor:
+    def __init__(self, num_bags, oob=False):
+        self.num_bags = num_bags
+        self.oob = oob
+        
+    def _generate_splits(self, data: np.ndarray):
+        '''
+        Generate indices for every bag and store in self.indices_list list
+        '''
+        self.indices_list = []
+        data_length = len(data)
+        for bag in range(self.num_bags):
+            # Your Code Here
+            indices = np.random.choice(data_length, size=data_length, replace=True)
+            self.indices_list.append(indices)
+        
+    def fit(self, model_constructor, data, target):
+        '''
+        Fit model on every bag.
+        Model constructor with no parameters (and with no ()) is passed to this function.
+        
+        example:
+        
+        bagging_regressor = SimplifiedBaggingRegressor(num_bags=10, oob=True)
+        bagging_regressor.fit(LinearRegression, X, y)
+        '''
+        self.data = None
+        self.target = None
+        self._generate_splits(data)
+        assert len(set(list(map(len, self.indices_list)))) == 1, 'All bags should be of the same length!'
+        assert list(map(len, self.indices_list))[0] == len(data), 'All bags should contain `len(data)` number of elements!'
+        self.models_list = []
+        for bag in range(self.num_bags):
+            model = model_constructor()
+            data_bag, target_bag = data[self.indices_list[bag]], target[self.indices_list[bag]]# Your Code Here
+            self.models_list.append(model.fit(data_bag, target_bag)) # store fitted models here
+        if self.oob:
+            self.data = data
+            self.target = target
+        
+    def predict(self, data):
+        '''
+        Get average prediction for every object from passed dataset
+        '''
+        # Your code here
+        predictions = [model.predict(data) for model in self.models_list]
+        return np.mean(predictions, axis=0)
+    
+    def _get_oob_predictions_from_every_model(self):
+        '''
+        Generates list of lists, where list i contains predictions for self.data[i] object
+        from all models, which have not seen this object during training phase
+        '''
+        list_of_predictions_lists = [[] for _ in range(len(self.data))]
+        # Your Code Here
+        for i, model in enumerate(self.models_list):
+            all_indices = np.arange(len(self.data))
+            oob_indices = np.setdiff1d(all_indices, self.indices_list[i])
+            oob_predictions = model.predict(self.data[oob_indices])
+
+            for idx, pred in zip(oob_indices, oob_predictions):
+                list_of_predictions_lists[idx].append(pred)
+        
+        self.list_of_predictions_lists = np.array(list_of_predictions_lists, dtype=object)
+    
+    def _get_averaged_oob_predictions(self):
+        '''
+        Compute average prediction for every object from training set.
+        If object has been used in all bags on training phase, return None instead of prediction
+        '''
+        self._get_oob_predictions_from_every_model()
+        self.oob_predictions = [np.mean(preds) if len(preds) > 0 else None for preds in self.list_of_predictions_lists]
+        
+        
+    def OOB_score(self):
+        '''
+        Compute mean square error for all objects, which have at least one prediction
+        '''
+        self._get_averaged_oob_predictions()
+        preds = np.array(self.oob_predictions)
+        mask = preds != None
+        return np.mean((self.target[mask] - preds[mask])**2)
---
+++ ml-projects/assignment0_01_knn/.ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,2 @@
+kNN implementation assignment:
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/Elvina642/ml-course/blob/1d2390fb763656356dada3d5340dcf6411fdc0ae/homeworks/assignment0_01_knn/knn_assignment_0_01.ipynb)
